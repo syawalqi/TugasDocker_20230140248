01db7668c67b71de33b08bfc1e94fb4959d56f8d
syawalqi <galihmaulanasyawalqi@gmail.com>
2026-04-13 10:27:03 +0700

    test: refactor test package to match project structure

--- src/test/java/com/tugas/deploy/DeployApplicationTests.java
+++ src/test/java/com/tugas/deploy/DeployApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.tugas.deploy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DeployApplicationTests {
 
 	@Test
 	void contextLoads() {
